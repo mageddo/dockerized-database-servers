937f18280a03683000ba55872ce289cbc38c7305
Elvis de Freitas <edigitalb@gmail.com>
2017-04-08 11:06:40 -0300

    setup php server

---
+++ php/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -e
+
+#chown -R www-data:www-data /var/www/html
+chgrp -R www-data /var/www/html
+
+if [ -z "$1" ]
+then
+    exec apache2-foreground
+else
+    exec "$@"
+fi
+
+#EOF
